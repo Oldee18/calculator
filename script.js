fc04fb2863b3d1db1943872c8ff314b9993a4f39
Oldee18 <oldee18@gmail.com>
2025-01-18 09:54:08 +0100

    Added an else if condition for the operator buttons, and an event listener

--- script.js
+++ script.js
@@ -6,22 +6,30 @@ const clearButton = document.querySelector(".clear")
 const deleteButton = document.querySelector(".delete")
 const operatorButtons = document.querySelectorAll(".operator")
 
+operatorButtons.forEach((operatorBtn) => {
+    operatorBtn.addEventListener("click", () => {
+        if (operatorBtn != "") {
+            display.textContent = ""
+        }
+    })
+}) 
+
+
+
 numberButtons.forEach((numberBtn) => {
     numberBtn.addEventListener("click", () => {
         if (operatorButtons === "") {
-            firstNumber += button.textContent
+            firstNumber += numberBtn.textContent
             display.textContent = firstNumber
-        } else {
-            secondNumber += button.textContent
+            console.log(firstNumber)
+        } else if (operatorButtons != "") {
+            secondNumber += numberBtn.textContent
             display.textContent = secondNumber
+            console.log(secondNumber)
         }
     })
 })
 
-/* const operate = function () {
-    
-    console.table(operator) 
-} */
 
 let display = document.querySelector('.output-field')
 
