f9b3f5eef226d11502a081bc3e78fffc05f22c93
Oldee <oldee18@gmail.com>
2025-01-20 15:04:40 +0100

    place the operator button into the number buttons function

--- script.js
+++ script.js
@@ -7,24 +7,22 @@ const deleteButton = document.querySelector(".delete")
 const operatorButtons = document.querySelectorAll(".operator")
 let display = document.querySelector('.output-field')
 
-operatorButtons.forEach((operatorBtn) => {
-    operatorBtn.addEventListener("click", () => {
-            display.textContent = ""
-    })
-}) 
-
-
-
 numberButtons.forEach((numberBtn) => {
     numberBtn.addEventListener("click", () => {
-        if (operatorButtons === "") {
-            firstNumber += numberBtn.textContent
-            display.textContent = firstNumber
-        } else 
-            secondNumber += numberBtn.textContent
-            display.textContent = secondNumber
+    	firstNumber += numberBtn.textContent
+     	display.textContent = firstNumber
         })
-})
+		operatorButtons.forEach((operatorBtn) => {
+			operatorBtn.addEventListener("click", () => {
+					display.textContent = ""
+					secondNumber += numberBtn.textContent
+			})
+		}) 
+	})
+
+
+
+	
 
 
 
