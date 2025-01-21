a548a266c53ed8e5659de9933eebf54f86f88688
Oldee18 <oldee18@gmail.com>
2025-01-21 21:08:40 +0100

    Started a new fresh approach.

--- script.js
+++ script.js
@@ -1,30 +1,54 @@
-let firstNumber = ""
-let secondNumber = ""
+let firstNumber = 0
+let secondNumber = 0
+
+const add = function () {
+	console.log(firstNumber + secondNumber)
+}
+
+const subtract = function () {
+	console.log(firstNumber - secondNumber)
+}
+
+const multiply = function () {
+	console.log(firstNumber * secondNumber)
+}
+
+const divide = function () {
+	console.log(firstNumber / secondNumber)
+}
+
 const numberButtons = document.querySelectorAll(".number")
-const equalsButton = document.querySelector(".equals")
-const clearButton = document.querySelector(".clear")
-const deleteButton = document.querySelector(".delete")
+const display = document.querySelector(".output-field")
 const operatorButtons = document.querySelectorAll(".operator")
-let display = document.querySelector('.output-field')
 
-numberButtons.forEach((numberBtn) => {
-    numberBtn.addEventListener("click", () => {
-    	firstNumber += numberBtn.textContent
-     	display.textContent = firstNumber
-        })
-		operatorButtons.forEach((operatorBtn) => {
-			operatorBtn.addEventListener("click", () => {
-					display.textContent = ""
-					secondNumber += numberBtn.textContent
-			})
-		}) 
-	})
-
-
-
-	
-
-
-
-console.log(operatorButtons)
+const operate = function () {
+	let num1 = +prompt("What is your first number?")
+	let num2 = +prompt("What is your second number?")
+	let operator = prompt("What operation do you want to finish? Add, Subtract, Multiply, Divide")
+		firstNumber += num1
+		secondNumber += num2
+			if (operator === "Add") {			
+				add()
+			} else if (operator === "Subtract") {
+				subtract()
+			} else if (operator === "Multiply") {
+				multiply()
+			} else if (operator === "Divide") {
+				divide()
+			} else 
+				alert("Bad operator choice. Please choose a valid operator!")
+}
 console.log(numberButtons)
+console.log(operatorButtons)
+const displayPopulate = function () {
+	numberButtons.forEach((numberBtn) => {
+		numberBtn.addEventListener("click", () => {
+			display.textContent += numberBtn.textContent
+			firstNumber += numberBtn.textContent
+		})
+		
+	})
+}
+
+
+displayPopulate()
